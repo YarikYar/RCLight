f1794cf459abf63376bca13c10d4d56fc4a6b8dd
YarikYar <yaryarikyar@ya.ru>
2017-03-05 21:52:43 +0300

    Calibration
    Added initial calibration and empty interrupt routines

--- User/main.c
+++ User/main.c
@@ -2,10 +2,41 @@
 #include "defines.h"
 #include "delay.h"
 #include "func.h"
+#include "config.h"
 
+#define FLAG_LEFT   (1<<0)
+#define FLAG_RIGHT  (1<<1)
+#define FLAG_LIGHT  (1<<2)
+#define FLAG_BACK   (1<<3)
+#define FLAG_STOP   (1<<4)
+#define FLAG_WARN   (1<<5)
+#define FLAG_STROB  (1<<6)
+
+#define ACTION_IDLE    (1<<0)
+#define ACTION_MEASURE (1<<1)
+#define ACTION_UPDATE  (1<<2)
+#define ACTION_BUTTON  (1<<3)
+
+uint8_t action, flag;
+
+
+void EXTI0_1_IRQHandler(void) // Button interrupt routine
+{
+   EXTI->PR =0x02; 
+}
+
+void TIM17_IRQHandler(void)  // Update timer interrupt routine
+{
+	TIM17->SR = 0;
+}
 
 int __attribute__((noreturn)) main(void)
 {
+	/* Local variables definition */
+	uint16_t setup_th, setup_st;  // Initial calibration values will be stored here
+	uint16_t cur_th, cur_st;      // Current channel duty value will be here
+	/* -------------------------- */
+	
 	/* ------------------------------------------------------------------ */
 	system_clock();   // Initing PLL from 8MHz/2 HSI with x12 multiplier
 	init_delay();     // Initing delay functions (using TIM2)
@@ -32,7 +63,7 @@ int __attribute__((noreturn)) main(void)
 	SYSCFG->EXTICR[0] = SYSCFG_EXTICR1_EXTI1_PB;
 	EXTI->IMR = EXTI_IMR_MR1 ;
 	EXTI->FTSR = EXTI_FTSR_TR1 ;
-//	NVIC_EnableIRQ(EXTI0_1_IRQn);
+	NVIC_EnableIRQ(EXTI0_1_IRQn);
 	/* ---------- */
 	
 	/* Timers setup */
@@ -41,7 +72,7 @@ int __attribute__((noreturn)) main(void)
 	    TIM17->DIER |= TIM_DIER_UIE;
 	    TIM17->CR1 |= TIM_CR1_CEN;
 			NVIC_SetPriority(TIM17_IRQn, 1);
-	 //   NVIC_EnableIRQ(TIM17_IRQn);
+	    NVIC_EnableIRQ(TIM17_IRQn);
 			/* -------------------- */
 			
 			/* TIM14 - PWM timer */
@@ -58,8 +89,13 @@ int __attribute__((noreturn)) main(void)
 			TIM1->PSC = 48-1;
 			/* ---------------------- */
 	/* ------------ */                                 
-	TIM14->CCR1  =0;                                                                                                                                                                                                                                               
-	
+	    
+	/* Initial calibration */
+	while(pulsein(0) < 1000){}  // This delay is neccessary to wait for stable signal
+	 setup_th = pulsein(0);
+	while(pulsein(1) < 1000){}
+	 setup_st = pulsein(1);
+	/* ------------------- */
 	while(1)
 	{
 		
